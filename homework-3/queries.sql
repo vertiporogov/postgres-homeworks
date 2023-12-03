9328cd9527ebc2efd31b33326c2580b027ec1dca
alexander <vertiporogov.alexandr@mail.ru>
2023-12-03 12:46:35 +0300

    homework-3 third

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -21,6 +21,11 @@ ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
+SELECT company_name FROM customers
+LEFT JOIN orders USING(customer_id)
+EXCEPT
+SELECT company_name FROM customers
+RIGHT JOIN orders USING(customer_id)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
