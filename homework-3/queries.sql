73cd3e391f1af76a77d59ba3fbec8947e1294aa1
alexander <vertiporogov.alexandr@mail.ru>
2023-12-03 11:51:47 +0300

    homework-3 first

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,6 +2,11 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+SELECT customers.company_name AS customer, CONCAT(first_name, ' ', last_name) AS employee FROM customers
+JOIN orders USING(customer_id)
+JOIN employees USING(employee_id)
+JOIN shippers ON orders.ship_via=shippers.shipper_id
+WHERE employees.city = 'London' AND customers.city = 'London' AND shippers.company_name = 'United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
