66c96805575c532cffbd008041ef1780247a511f
alexander <vertiporogov.alexandr@mail.ru>
2023-12-03 23:54:36 +0300

    homework-4 na segodnя hvatit

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -1,9 +1,11 @@
 -- Подключиться к БД Northwind и сделать следующие изменения:
 -- 1. Добавить ограничение на поле unit_price таблицы products (цена должна быть больше 0)
 
+ALTER TABLE products ADD CONSTRAINT chk_products_unit_price CHECK(unit_price > 0)
 
 -- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
 
+ALTER TABLE products ADD CONSTRAINT chk_products_discontinued CHECK(discontinued IN (0, 1))
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
 
